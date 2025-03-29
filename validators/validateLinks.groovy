cc8bd3cdfbd351d189f8b8563bd3cb16156f4d32
Sergei Troshin <65363658+sergeyopypey@users.noreply.github.com>
2025-03-30 01:48:35 +0400

    Update validateLinks.groovy

--- validators/validateLinks.groovy
+++ validators/validateLinks.groovy
@@ -1,5 +1,8 @@
+package validators
+
 import webwork.action.ActionContext
 import com.atlassian.jira.issue.Issue
+import javax.servlet.http.HttpServletRequest
 import com.atlassian.jira.issue.IssueManager
 import com.atlassian.jira.issue.IssueFieldConstants
 import com.atlassian.jira.component.ComponentAccessor
@@ -10,17 +13,18 @@ final String ALLOWED_LINK_TYPE = "is blocked by"
 final Set<String> ALLOWED_ISSUE_TYPES = ["Bug", "Incident"]
 final IssueManager issueManager = ComponentAccessor.getIssueManager()
 
-Issue issue = (Issue) issue
-
-String issueLinksLinkType = ActionContext.request.getParameter(IssueLinksSystemField.PARAMS_LINK_TYPE)
-String[] issueLinkIssueKeys = ActionContext.request.getParameterValues(IssueLinksSystemField.PARAMS_ISSUE_KEYS)
+HttpServletRequest httpServletRequest = ActionContext.getRequest()
+String issueLinksLinkType = httpServletRequest.getParameter(IssueLinksSystemField.PARAMS_LINK_TYPE) ?:
+        httpServletRequest.getParameterValues(IssueLinksSystemField.PARAMS_LINK_TYPE)?.getAt(0)
+String[] issueLinkIssueKeys = httpServletRequest.getParameterValues(IssueLinksSystemField.PARAMS_ISSUE_KEYS)
 
 if (Objects.isNull(issueLinkIssueKeys)) {
-    throw new InvalidInputException("You must link at least one blocking bug")
+    throw new InvalidInputException(IssueFieldConstants.ISSUE_LINKS, "You must link at least one valid blocking issue type $ALLOWED_ISSUE_TYPES")
 }
 
+log.warn("IssueLinksLinkType: " + issueLinksLinkType)
 if (!ALLOWED_LINK_TYPE.equals(issueLinksLinkType)) {
-    throw new InvalidInputException(IssueFieldConstants.ISSUE_LINKS, "You must use 'is blocked by' link type")
+    throw new InvalidInputException(IssueFieldConstants.ISSUE_LINKS, "You must use '$ALLOWED_LINK_TYPE' link type")
 }
 
 boolean hasValidLinkIssues = issueLinkIssueKeys.any { issueLinkIssueKey ->
