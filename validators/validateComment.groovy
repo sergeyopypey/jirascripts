43aa3533453c87af047f2255a31cbbf6471c2450
Sergei Troshin <65363658+sergeyopypey@users.noreply.github.com>
2025-03-30 00:32:36 +0400

    Create validateComment.groovy

---
+++ validators/validateComment.groovy
@@ -0,0 +1,17 @@
+import webwork.action.ActionContext
+import com.atlassian.jira.issue.Issue
+import org.apache.commons.lang3.StringUtils
+import com.atlassian.jira.issue.IssueFieldConstants
+import com.opensymphony.workflow.InvalidInputException
+
+Issue issue = (Issue) issue
+
+String resolutionName = issue.resolution?.name
+
+if (resolutionName.equals("Rejected")) {
+    String comment = ActionContext.request.getParameter(IssueFieldConstants.COMMENT)
+
+    if (StringUtils.isBlank(comment)) {
+        throw new InvalidInputException(IssueFieldConstants.COMMENT, "Please specify comment")
+    }
+}
